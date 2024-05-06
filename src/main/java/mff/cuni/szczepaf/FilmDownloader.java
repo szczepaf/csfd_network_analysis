a9d3c2ed14737590f41d9a143cf011266d3b508f
szczepaf <szczjr@gmail.com>
2024-05-06 16:48:03 +0200

    Add implementation of FilmDownloader

--- src/main/java/mff/cuni/szczepaf/FilmDownloader.java
+++ src/main/java/mff/cuni/szczepaf/FilmDownloader.java
@@ -1,10 +1,50 @@
 package mff.cuni.szczepaf;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+
+/**
+ * FilmDownloader is a class that downloads film information from a given URL.
+ */
 public class FilmDownloader implements IDownloader {
+
+    /**
+     * Downloads film data in JSON format from the specified URL of a CSFD movie.
+     * The JSON data is embedded in a tag "application/ld+json".
+     * @param url The URL of the movie webpage.
+     * @return A String containing the JSON data of the film. Null if the data is not found.
+     */
+
+    static String structureURLIDPrefix = "https://www.csfd.cz/film/";
+    @Override
     public String downloadByURL(String url) {
-        return "Film data";
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Element script = doc.selectFirst("script[type=application/ld+json]");
+
+            if (script != null) {
+                return script.data();
+            } else {
+                return null;
+            }
+        } catch (IOException e) {
+            // Catch the IOException from Jsoup get method
+            System.out.println("Unable to download the JSON with information about the movie.");
+            System.out.println(e);
+            return null;
+        }
     }
+
+    /**
+     * Downloads and extracts film data in JSON format by film ID.
+     * @param id The ID of the film to download.
+     * @return A String containing the JSON data of the film.
+     */
+    @Override
     public String downloadByID(String id) {
-        return "Film data by ID";
+        return this.downloadByURL(structureURLIDPrefix + id);
     }
 }
--- src/main/java/mff/cuni/szczepaf/Main.java
+++ src/main/java/mff/cuni/szczepaf/Main.java
@@ -1,5 +1,19 @@
 package mff.cuni.szczepaf;
 
+public class Main {
 
+    public static void main(String[] args) {
+        FilmDownloader downloader = new FilmDownloader();
 
+        String ID = "1096199";
 
+        String jsonData = downloader.downloadByID(ID);
+
+        if (jsonData != null) {
+            System.out.println("JSON Data:");
+            System.out.println(jsonData);
+        } else {
+            System.out.println("No JSON data found at the specified URL.");
+        }
+    }
+}
