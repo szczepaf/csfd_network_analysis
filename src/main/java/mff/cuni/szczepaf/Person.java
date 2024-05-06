224bd8d97216f91ee814b1247033643c9314e54b
szczepaf <szczjr@gmail.com>
2024-05-06 18:03:08 +0200

    Add equals and hashcode implementations for Person class

--- src/main/java/mff/cuni/szczepaf/Person.java
+++ src/main/java/mff/cuni/szczepaf/Person.java
@@ -11,4 +11,29 @@ public class Person implements IMediaEntity{
     }
 
     private String name;
+
+
+    @Override
+    public int hashCode() {
+        // Hash the name, if it is null, return some constant, e.g. 0
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Let's assume that two people are identical if they share the same name.
+        // That is of course not ideal, but at the same time, CSFD is strict about having differences in names for different actors.
+        // Let's rely on them to be consistent.
+
+        if (obj == this) {
+            return true;
+        }
+
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Person person = (Person) obj;
+        return name.equals(person.name);
+    }
 }
